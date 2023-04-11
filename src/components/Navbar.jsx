cc2173dd70cc88fe1fd0d013fd1ab032b65c03ac
erol23 <hurkanrol15@gmail.com>
2023-04-11 13:02:59 +0200

    edited navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import Logo from "../assets/logo-dark.png";
 import { useState } from "react";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
-    const handleClick = () => {
-        setNav(!nav)
-    }
+  const handleClick = () => {
+    setNav(!nav);
+  };
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
@@ -22,19 +22,23 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
 
-      <div className="md:hidden z-10" onClick={handleClick}>
-        <FaBars />
+      <div className="md:hidden z-10 hover:cursor-pointer" onClick={handleClick}>
+        {nav ? <FaTimes /> : <FaBars />}
       </div>
 
-      <div className={nav ? "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center": "hidden"}>
-        <ul>
-          <li className="py-6 text-4xl">Home</li>
-          <li className="py-6 text-4xl">About</li>
-          <li className="py-6 text-4xl">Skils</li>
-          <li className="py-6 text-4xl">Project</li>
-          <li className="py-6 text-4xl">Contact</li>
-        </ul>
-      </div>
+      <ul
+        className={
+          nav
+            ? "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+            : "hidden"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skils</li>
+        <li className="py-6 text-4xl">Project</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
     </div>
   );
 };
