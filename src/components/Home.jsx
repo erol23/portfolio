9287005b38a390480965f0b0e45eb08961927040
erol23 <hurkanrol15@gmail.com>
2023-04-11 14:38:27 +0200

    navbar finished

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import {HiArrowNarrowRight} from "react-icons/hi"
+
+const Home = () => {
+  return (
+    <div className="bg-[#0a192f] w-full h-screen">
+
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,17 @@
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo-dark.png";
 import { useState } from "react";
+
+const icons = [
+  { name: "Linkedin", icon: <FaLinkedin size={25}/>, bgColor: "bg-blue-600" },
+  { name: "GitHub", icon: <FaGithub size={25}/>, bgColor: "bg-slate-800" },
+  { name: "EMail", icon: <HiOutlineMail size={25}/>, bgColor: "bg-emerald-400" },
+  { name: "Contect", icon: <BsFillPersonLinesFill size={25}/>, bgColor: "bg-gray-600" },
+];
+
+icons.map((icon) => console.log(icon.bgColor))
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -22,7 +33,10 @@ const Navbar = () => {
         <li>Contact</li>
       </ul>
 
-      <div className="md:hidden z-10 hover:cursor-pointer" onClick={handleClick}>
+      <div
+        className="md:hidden z-10 hover:cursor-pointer"
+        onClick={handleClick}
+      >
         {nav ? <FaTimes /> : <FaBars />}
       </div>
 
@@ -39,6 +53,21 @@ const Navbar = () => {
         <li className="py-6 text-4xl">Project</li>
         <li className="py-6 text-4xl">Contact</li>
       </ul>
+
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          {icons.map((icon) => (
+            <li className={`w-[130px] h-[50px] flex justify-between items-center ml-[-83px] hover:ml-[-10px] duration-300 ${icon.bgColor}`}>
+              <a
+                className='flex justify-between items-center w-full text-gray-300'
+                href="/"
+              >
+                {icon.name} {icon.icon}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
