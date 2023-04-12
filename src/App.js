6611be954c1cc8656dc3af36663e1692d9a0c060
erol23 <hurkanrol15@gmail.com>
2023-04-12 16:48:36 +0200

    added projects component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
