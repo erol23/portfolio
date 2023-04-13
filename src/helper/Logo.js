96dcb5b37a8050a83066a0f716e1ff90f64d3779
erol23 <hurkanrol15@gmail.com>
2023-04-13 16:22:57 +0200

    added projectsData

--- src/helper/Logo.js
+++ src/helper/Logo.js
@@ -1,61 +1,76 @@
-import HTML from "../assets/html.png"
-import CSS from "../assets/css.png"
-import JavaScript from "../assets/javascript.png"
-import React from "../assets/react.png"
-import Redux from "../assets/redux.png"
-import Tailwind from "../assets/tailwind.png"
-import Bootstrap from "../assets/bootstrap.png"
-import FireBase from "../assets/firebase.png"
-import GitHub from "../assets/github.png"
-import Python from "../assets/python.png"
-import { FaGithub, FaLinkedin } from "react-icons/fa"
-import { HiOutlineMail } from "react-icons/hi"
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import React from "../assets/react.png";
+import Redux from "../assets/redux.png";
+import Tailwind from "../assets/tailwind.png";
+import Bootstrap from "../assets/bootstrap.png";
+import FireBase from "../assets/firebase.png";
+import GitHub from "../assets/github.png";
+import Python from "../assets/python.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 export const skillsData = [
-    {
-        name: "HTML",
-        img: HTML
-    },
-    {
-        name: "CSS",
-        img: CSS
-    },
-    {
-        name: "JavaScript",
-        img: JavaScript
-    },
-    {
-        name: "React",
-        img: React
-    },
-    {
-        name: "Redux",
-        img: Redux
-    },
-    {
-        name: "Tailwind",
-        img: Tailwind
-    },
-    {
-        name: "Bootstrap",
-        img: Bootstrap
-    },
-    {
-        name: "FireBase",
-        img: FireBase
-    },
-    {
-        name: "GitHub",
-        img: GitHub
-    },
-    {
-        name: "Python",
-        img: Python
-    }
-]
+  {
+    name: "HTML",
+    img: HTML,
+  },
+  {
+    name: "CSS",
+    img: CSS,
+  },
+  {
+    name: "JavaScript",
+    img: JavaScript,
+  },
+  {
+    name: "React",
+    img: React,
+  },
+  {
+    name: "Redux",
+    img: Redux,
+  },
+  {
+    name: "Tailwind",
+    img: Tailwind,
+  },
+  {
+    name: "Bootstrap",
+    img: Bootstrap,
+  },
+  {
+    name: "FireBase",
+    img: FireBase,
+  },
+  {
+    name: "GitHub",
+    img: GitHub,
+  },
+  {
+    name: "Python",
+    img: Python,
+  },
+];
 
 export const icons = [
-    { name: "Linkedin", url: "https://www.linkedin.com/in/huseyinerkanerol/", icon: <FaLinkedin size={25}/>, bgColor: "bg-blue-600" },
-    { name: "GitHub", url:"https://github.com/erol23", icon: <FaGithub size={25}/>, bgColor: "bg-slate-800" },
-    { name: "EMail", url:"mailto:hurkanrol15@gmail.com", icon: <HiOutlineMail size={25}/>, bgColor: "bg-emerald-400" },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/huseyinerkanerol/",
+    icon: <FaLinkedin size={25} />,
+    bgColor: "bg-blue-600",
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/erol23",
+    icon: <FaGithub size={25} />,
+    bgColor: "bg-slate-800",
+  },
+  {
+    name: "EMail",
+    url: "mailto:hurkanrol15@gmail.com",
+    icon: <HiOutlineMail size={25} />,
+    bgColor: "bg-emerald-400",
+  },
 ];
---
+++ src/helper/Projects.js
@@ -0,0 +1,31 @@
+import foodApp from "../assets/food-app.png";
+import movieApp from "../assets/movie-app.png";
+import netflix from "../assets/netflix-tw.png";
+import tourApp from "../assets/tour-app.png";
+
+export const ProjectsData = [
+  {
+    name: "Netflix-Tw",
+    url: "https://tw-netflix.vercel.app/",
+    codeUrl: "https://github.com/erol23/tw-netflix",
+    photo: netflix,
+  },
+  {
+    name: "Movie-App",
+    url: "https://erol23.github.io/movie-app/",
+    codeUrl: "https://github.com/erol23/movie-app",
+    photo: movieApp,
+  },
+  {
+    name: "Food-App",
+    url: "https://erol23.github.io/recipeapp/",
+    codeUrl: "https://github.com/erol23/recipeapp",
+    photo: foodApp,
+  },
+  {
+    name: "Tour-App",
+    url: "https://erol23.github.io/Tour-Page/",
+    codeUrl: "https://github.com/erol23/Tour-Page",
+    photo: tourApp,
+  },
+];
